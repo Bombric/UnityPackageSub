d56ba1656ca39fc8d18c5a5a4c2fecb13672b8f1
Bombric <76108917+Bombric@users.noreply.github.com>
2021-09-02 10:10:09 +0300

    Add files via upload
    Init package

---
+++ Runtime/FirstChanges/SomeCode.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //some change
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
